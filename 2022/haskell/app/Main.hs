e3e55939be230dc7b3eae88fa401a28c499cc248
benluelo <benluelo@hotmail.com>
2022-12-02 03:56:35 +0000

    haskell/1-1

--- 2022/haskell/app/Main.hs
+++ 2022/haskell/app/Main.hs
@@ -1,3 +1,16 @@
 module Main where
+
+import Data.Function
+import Text.Read
+
 main :: IO ()
-main = putStrLn "Hello World!"
+main = do
+  fileLines <- readFile "../inputs/day-1.txt"
+  lines fileLines & map readMaybe & foldl (accumulate) [] & maximum & print
+
+accumulate :: [Integer] -> Maybe Integer -> [Integer]
+accumulate acc curr = case (acc, curr) of
+  ([], Just curr) -> [curr]
+  ([], Nothing) -> []
+  (acc : finished, Just curr) -> acc + curr : finished
+  (accumulated, Nothing) -> 0 : accumulated
