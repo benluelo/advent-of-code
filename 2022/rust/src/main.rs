f20a0784a6bb2d0f526198702cbb725e7b4fe1b6
benluelo <benluelo@hotmail.com>
2022-12-02 04:59:06 +0000

    Bootstrapping Rust project

---
+++ 2022/rust/src/main.rs
@@ -0,0 +1,44 @@
+#![feature(concat_idents)]
+
+macro_rules! print_solutions {
+    ($([$day:ident $(, $part_2:ident)?]),*) => {
+        $(
+            {
+                use $day::*;
+
+                let input = std::fs::read_to_string(
+                    std::path::PathBuf::from_iter([
+                        "..",
+                        "inputs",
+                        &stringify!($day).replace('_', "-")
+                    ])
+                ).unwrap();
+
+                let day_number = &stringify!($day)
+                    .split("-")
+                    .nth(2)
+                    .unwrap();
+
+                println!(
+                    "{}-1 solution: {}",
+                    day_number,
+                    solution(input.clone())
+                );
+
+                $(
+                    println!(
+                        "{}-2 solution: {}",
+                        day_number,
+                        concat_idents!(solution, _, $part_2) (input)
+                    );
+                )?
+            }
+        )*
+    }
+}
+
+fn main() {
+    print_solutions! {
+        [day_1]
+    }
+}
