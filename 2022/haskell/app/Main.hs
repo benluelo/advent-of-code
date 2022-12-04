1332d6f1b410d668d0286b270721c140f62865c1
benluelo <benluelo@hotmail.com>
2022-12-04 02:20:12 +0000

    feat(solution): haskell/2022/3-1

--- 2022/haskell/app/Main.hs
+++ 2022/haskell/app/Main.hs
@@ -2,63 +2,112 @@ module Main where
 
 import Data.Function
 import Data.List
+import qualified Data.Set as Set
+import Data.Char (isUpper, isLower, isSpace)
 import Text.Read
 import System.Directory
 import System.FilePath
+import Debug.Trace
 
 main :: IO ()
 main = do
   fileNames <- listDirectory "../inputs/"
-  let qualifiedFileNames = map ("../inputs/" ++) fileNames 
-  mapM_ print qualifiedFileNames
+  let qualifiedFileNames = map ("../inputs/" ++) fileNames
+  -- mapM_ print qualifiedFileNames
   fileContents <- mapM getDayAndInput qualifiedFileNames
-  let final = fileContents & sortBy (\(a, _) (b, _) -> a `compare` b) & map runDay
+  let
+    final = fileContents
+      & sortBy (\(a, _) (b, _) -> a `compare` b)
+      & map runDay
   mapM_ print final 
 
-getDayAndInput :: FilePath -> IO (Integer, String)
+getDayAndInput :: FilePath -> IO (Int, String)
 getDayAndInput fp = do
   contents <- readFile fp
   return ((getDayFromPath fp), contents)
 
 -- |split `day-N` into an integer `N`.
-getDayFromPath :: FilePath -> Integer
+getDayFromPath :: FilePath -> Int
 getDayFromPath fp = splitAt 4 (takeBaseName fp) & snd & read
 
-data DayOutput = DayOutput {
-  day :: Integer,
-  part1 :: String,
-  part2 :: String
-}
+data DayOutput = Unimplemented Int
+  | DayOutput {
+    day :: Int,
+    part1 :: String,
+    part2 :: String
+  }
 
 instance Show DayOutput where
-  show output = "day " ++ show (day output) ++ "\n"
-                ++ "part 1: " ++ part1 output ++ "\n"
-                ++ "part 2: " ++ part2 output ++ "\n"
+  show (DayOutput {
+    day = day,
+    part1 = part1,
+    part2 = part2
+  }) = "day " ++ show (day) ++ "\n"
+                ++ "part 1: " ++ part1 ++ "\n"
+                ++ "part 2: " ++ part2 ++ "\n"
+  show (Unimplemented day) = "day " ++ show (day) ++ " not yet implemented\n"
 
-runDay :: (Integer, String) -> DayOutput
+runDay :: (Int, String) -> DayOutput
 
 runDay (1, input) = DayOutput {
   day = 1,
-  part1 = parse input
+  part1 = input
+    & parse
     & sortBy (flip compare)
     & maximum
     & show,
-  part2 = parse input
+  part2 = input
+    & parse
     & take 3
     & foldr (+) 0
     & show
 } where
-    parse :: String -> [Integer] 
+    parse :: String -> [Int] 
     parse input =
       lines input
       & map readMaybe
       & foldl (accumulate) []
 
-    accumulate :: [Integer] -> Maybe Integer -> [Integer]
+    accumulate :: [Int] -> Maybe Int -> [Int]
     accumulate acc curr = case (acc, curr) of
       ([], Just curr) -> [curr]
       ([], Nothing) -> []
       (acc : finished, Just curr) -> acc + curr : finished
       (accumulated, Nothing) -> 0 : accumulated
 
-runDay (dayNum, input) = undefined ("day " ++ show (dayNum) ++ " not yet implemented")
+runDay (3, input) = DayOutput {
+  day = 3,
+  part1 = input
+    & parse
+    & map (getDuplicate . (mapTuple $ Set.map (priority)))
+    & sum
+    & show,
+  part2 = ""
+} where
+    parse :: String -> [(Set.Set Char, Set.Set Char)] 
+    parse input = input
+      & lines
+      & map (
+        mapTuple Set.fromList
+        . mapTuple (filter $ not . isSpace)
+        . splitInHalf
+      )
+
+    getDuplicate :: (Set.Set Int, Set.Set Int) -> Int
+    getDuplicate (a, b) = case Set.toList $ Set.intersection a b of
+      [x] -> x
+      _ -> error ("bad input: " ++ show a ++ show b)
+
+    mapTuple :: Show a => Show b => (a -> b) -> (a, a) -> (b, b)
+    mapTuple f (a1, a2) = {- traceShow (a1, a2) -} (f a1, f a2)
+    
+    priority :: Char -> Int
+    priority c = case c of
+      c | isUpper c -> fromEnum c - 38
+      c | isLower c -> fromEnum c - 96
+      _ -> error ("bad input: '" ++ ['\'', c])
+
+    splitInHalf :: [Char] -> ([Char], [Char])
+    splitInHalf s = splitAt (length s `div` 2) s
+
+runDay (dayNum, input) = Unimplemented dayNum
