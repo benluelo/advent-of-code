4e54409f283d4d05f6bf21f5e94d00bd476f636d
benluelo <benluelo@hotmail.com>
2022-12-05 23:12:57 +0000

    feat(solution): rust/2022/4-2

--- 2022/rust/src/day_4.rs
+++ 2022/rust/src/day_4.rs
@@ -1,4 +1,22 @@
+use std::ops::RangeInclusive;
+
 pub fn solution(input: String) -> usize {
+    parse(&input, |[range_a, range_b]| {
+        (range_a.contains(range_b.start()) && range_a.contains(range_b.end()))
+            || (range_b.contains(range_a.start()) && range_b.contains(range_a.end()))
+    })
+}
+
+pub fn solution_part_2(input: String) -> usize {
+    parse(&input, |[range_a, range_b]| {
+        range_a.contains(range_b.start())
+            || range_a.contains(range_b.end())
+            || range_b.contains(range_a.start())
+            || range_b.contains(range_a.end())
+    })
+}
+
+fn parse(input: &str, filter_fn: fn(&[RangeInclusive<u32>; 2]) -> bool) -> usize {
     input
         .lines()
         .map(|line| {
@@ -14,9 +32,6 @@ pub fn solution(input: String) -> usize {
                 })
                 .map(|[a, b]| a..=b)
         })
-        .filter(|[range_a, range_b]| {
-            (range_a.contains(range_b.start()) && range_a.contains(range_b.end()))
-                || (range_b.contains(range_a.start()) && range_b.contains(range_a.end()))
-        })
+        .filter(filter_fn)
         .count()
 }
