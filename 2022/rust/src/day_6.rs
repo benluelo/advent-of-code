4f17ae08cc7d92a8e6ae97c80b2c15c347bfc903
benluelo <benluelo@hotmail.com>
2022-12-07 22:39:45 +0000

    feat(solution): rust/2022/6-2

--- 2022/rust/src/day_6.rs
+++ 2022/rust/src/day_6.rs
@@ -1,13 +1,24 @@
-pub fn solution(input: String) -> usize {
-    const ARRAY_SIZE: usize = 4;
+const START_OF_PACKET_MARKER_LENGTH: usize = 4;
+const START_OF_MESSAGE_MARKER_LENGTH: usize = 14;
 
+pub fn solution(input: String) -> usize {
+    parse::<START_OF_PACKET_MARKER_LENGTH>(&input)
+}
+
+pub fn solution_part_2(input: String) -> usize {
+    parse::<START_OF_MESSAGE_MARKER_LENGTH>(&input)
+}
+
+fn parse<const MARKER_LENGTH: usize>(input: &str) -> usize {
     input
         .trim()
         .as_bytes()
-        .array_windows()
-        .zip(ARRAY_SIZE..)
-        .find_map(|([a, b, c, d], i)| {
-            (a != b && a != c && a != d && b != c && b != d && c != d).then_some(i)
+        .array_windows::<MARKER_LENGTH>()
+        .zip(MARKER_LENGTH..)
+        .find_map(|(arr, i_window)| {
+            (1..MARKER_LENGTH)
+                .all(|i| !arr[i..].contains(&arr[i - 1]))
+                .then_some(i_window)
         })
         .unwrap()
 }
