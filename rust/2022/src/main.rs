7a0333f7cb7b8e5c9b081cc958720913b4ebd74c
benluelo <benluelo@hotmail.com>
2022-12-12 05:16:53 +0000

    feat(solution): rust/2022/10-1

--- rust/2022/src/main.rs
+++ rust/2022/src/main.rs
@@ -16,6 +16,8 @@ mod day_7;
 mod day_8;
 mod day_9;
 
+mod day_10;
+
 fn main() {
     print_solutions! {
         [1-1, 1-2]
@@ -26,6 +28,7 @@ fn main() {
         [7-1, 7-2]
         [8-1, 8-2]
         [9-1, 9-2]
+        [10-1]
     }
 }
 
