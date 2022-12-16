e0a48fbf2aa477b5867e69e21485c525cb0f48e3
benluelo <benluelo@hotmail.com>
2022-12-15 20:49:32 -0500

    feat(solution): rust/2022/11-2

--- rust/2022/src/day_11.rs
+++ rust/2022/src/day_11.rs
@@ -1,38 +1,46 @@
-use std::{cmp::Ordering, collections::BTreeMap};
+use std::{cmp::Ordering, collections::BTreeMap, ops::Rem};
 
-pub fn solution(input: &str) -> u32 {
+// 110220
+pub fn solution(input: &str) -> u64 {
+    parse_and_solve(input, 20, Some(3))
+}
+
+pub fn solution_part_2(input: &str) -> u64 {
+    parse_and_solve(input, 10_000, None)
+}
+
+fn parse_and_solve(
+    input: &str,
+    iterations: u32,
+    worry_level_reduction_after_inspection: Option<u32>,
+) -> u64 {
     let mut monkeys = input
         .split("\n\n")
         .map(Monkey::from_str)
         .collect::<BTreeMap<_, _>>();
 
-    for _ in 0..20 {
-        round(&mut monkeys);
+    let magic_number = monkeys
+        .values()
+        .fold(1, |acc, monkey| acc * monkey.test.divisible_by);
+
+    for _ in 0..iterations {
+        round(
+            &mut monkeys,
+            magic_number,
+            worry_level_reduction_after_inspection,
+        );
     }
 
-    let (highest, second_highest) = monkeys.values().map(|m| m.total_inspections).fold(
-        (0, 0),
-        |(highest, second_highest), curr| {
-            assert!(highest >= second_highest);
-
-            match curr.cmp(&second_highest) {
-                Ordering::Less | Ordering::Equal => (highest, second_highest),
-                Ordering::Greater => match curr.cmp(&highest) {
-                    Ordering::Less | Ordering::Equal => (highest, curr),
-                    Ordering::Greater => (curr, highest),
-                },
-            }
-        },
-    );
+    let (highest, second_highest) = get_2_most_active_monkeys(&monkeys);
 
     highest * second_highest
 }
 
 struct Monkey {
-    items: Vec<u32>,
+    items: Vec<u128>,
     operation: Operation,
     test: Test,
-    total_inspections: u32,
+    total_inspections: u64,
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
@@ -74,7 +82,7 @@ impl Monkey {
             Monkey {
                 items: items
                     .split(", ")
-                    .map(|s| s.parse::<u32>().unwrap())
+                    .map(|s| s.parse::<u128>().unwrap())
                     .collect(),
                 operation: match op {
                     "+" => Operation::Add(x.parse().unwrap()),
@@ -94,25 +102,56 @@ impl Monkey {
         )
     }
 
-    fn monkey_around(&mut self) -> impl Iterator<Item = (MonkeyId, u32)> + '_ {
-        self.items.drain(..).into_iter().map(|item| {
-            self.total_inspections += 1;
+    fn monkey_around(
+        &mut self,
+        modulo: u128,
+        worry_level_reduction_after_inspection: Option<u32>,
+    ) -> impl Iterator<Item = (MonkeyId, u128)> + '_ {
+        self.items
+            .drain(..)
+            .into_iter()
+            .map(|item| {
+                self.total_inspections += 1;
 
-            let worry_level = self.operation.worry_increase(item).div_floor(3);
-
-            (self.test.throw_to(worry_level), worry_level)
-        })
+                self.operation.worry_increase(item)
+            })
+            .map(move |worry_level| {
+                if let Some(reduction) = worry_level_reduction_after_inspection {
+                    worry_level.div_floor(reduction.into())
+                } else {
+                    worry_level
+                }
+                .rem(modulo)
+            })
+            .map(|worry_level| (self.test.throw_to(worry_level), worry_level))
     }
 }
 
+fn get_2_most_active_monkeys(monkeys: &BTreeMap<MonkeyId, Monkey>) -> (u64, u64) {
+    monkeys
+        .values()
+        .map(|m| m.total_inspections)
+        .fold((0, 0), |(highest, second_highest), curr| {
+            assert!(highest >= second_highest);
+
+            match curr.cmp(&second_highest) {
+                Ordering::Less | Ordering::Equal => (highest, second_highest),
+                Ordering::Greater => match curr.cmp(&highest) {
+                    Ordering::Less | Ordering::Equal => (highest, curr),
+                    Ordering::Greater => (curr, highest),
+                },
+            }
+        })
+}
+
 enum Operation {
-    Add(u32),
-    Mul(u32),
+    Add(u128),
+    Mul(u128),
     Square,
 }
 
 impl Operation {
-    fn worry_increase(&self, old: u32) -> u32 {
+    fn worry_increase(&self, old: u128) -> u128 {
         match self {
             Operation::Add(x) => x + old,
             Operation::Mul(x) => x * old,
@@ -121,14 +160,15 @@ impl Operation {
     }
 }
 
+#[derive(Clone)]
 struct Test {
-    divisible_by: u32,
+    divisible_by: u128,
     if_true: MonkeyId,
     if_false: MonkeyId,
 }
 
 impl Test {
-    fn throw_to(&self, x: u32) -> MonkeyId {
+    fn throw_to(&self, x: u128) -> MonkeyId {
         if x % self.divisible_by == 0 {
             self.if_true
         } else {
@@ -137,13 +177,19 @@ impl Test {
     }
 }
 
-fn round(monkeys: &mut BTreeMap<MonkeyId, Monkey>) {
+fn round(
+    monkeys: &mut BTreeMap<MonkeyId, Monkey>,
+    modulo: u128,
+    worry_level_reduction_after_inspection: Option<u32>,
+) {
     let keys = monkeys.keys().copied().collect::<Vec<_>>();
 
     for key in keys {
         let mut monkey = monkeys.remove(&key).unwrap();
 
-        for (destination_monkey, item) in monkey.monkey_around() {
+        for (destination_monkey, item) in
+            monkey.monkey_around(modulo, worry_level_reduction_after_inspection)
+        {
             monkeys
                 .get_mut(&destination_monkey)
                 .unwrap()
